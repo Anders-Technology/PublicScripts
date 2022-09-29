8350876ad0653a9d81e4f2304ee0ecba2c7db675
Cory Gegg <82197611+AndersTechnology@users.noreply.github.com>
2022-09-29 00:20:59 -0500

    Create UninstallDellOptimizer.ps1

---
+++ UninstallDellOptimizer.ps1
@@ -0,0 +1,17 @@
+#stop dell optimizer services
+Get-Service -Name "DellOptimizer" | Stop-Service
+
+
+
+#Get Dell Optimizer from registry
+$uninstallstring = get-itemproperty 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*' | Where-Object { $_.DisplayName -match "optimizer service"}
+
+
+
+#get the uninstall string for dell optimizer
+$uninstallstring = $removeapp.UninstallString
+
+
+
+#add the silent parameter and run throguh cmd to uninstall
+cmd /c $uninstallstring -silent
